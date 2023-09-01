5a307eb8fd9546add6c9c3e3bef30981d9ee086c
OSCAR ALBERTO MORA M�NDEZ <oscarmora18268@correo.itm.edu.co>
2023-09-01 14:37:50 -0500

    inicio del programa

---
+++ comision/Program.cs
@@ -0,0 +1,15 @@
+﻿//Un vendedor recibe un sueldo base más un 10% extra por comisión de sus ventas, el vendedor desea
+//saber cuánto dinero obtendrá por concepto de comisiones por las tres ventas que realiza en el
+//mes y el total que recibirá en el mes tomando en cuenta su sueldo base y comisiones.
+//DONE
+
+
+// 1 clase y motodo
+
+public class Comision
+{
+    static void Main(string[] Args)
+    {
+        //2 
+    }
+}
