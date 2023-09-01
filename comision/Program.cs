1a551fec0c61e69bcf2af813e60bf7d1a33b2032
OSCAR ALBERTO MORA M�NDEZ <oscarmora18268@correo.itm.edu.co>
2023-09-01 14:58:18 -0500

    Terminado

--- comision/Program.cs
+++ comision/Program.cs
@@ -10,6 +10,33 @@ public class Comision
 {
     static void Main(string[] Args)
     {
-        //2 
+        //2 declaracion de variables
+
+        double sueldoBase, venta1, venta2, venta3, sueldoTotal, comisionTotal;
+
+        // 3 recolectar los datos
+
+        Console.Write("Escriba su sueldo base: ");
+        sueldoBase = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Cual fue su primera venta: ");
+        venta1 = Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine("Cual fue su segunda venta: ");
+        venta2 = Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine("Cual fue su tercera venta: ");
+        venta3 = Convert.ToDouble(Console.ReadLine());
+
+        // 4 realizar los calculos}
+
+        comisionTotal = ((venta1 + venta2 + venta3) * 0.1);
+        sueldoTotal = comisionTotal + sueldoBase;
+
+        // Mostar resultados en pantalla
+
+        Console.WriteLine("el concepto por comision es: " + comisionTotal);
+        Console.WriteLine("Neto a pagar por este mes es: " + sueldoTotal);
+
     }
 }
